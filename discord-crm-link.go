a962217dfd3955237d81189f498b78c6fc098790
Lee Ingram <lee@soylentcola.com>
2016-11-07 17:47:53 +0000

    Added basic discord auth functionality

--- discord-crm-link.go
+++ discord-crm-link.go
@@ -1,7 +1,21 @@
 package main
 
 import "fmt"
+import "github.com/bwmarrin/discordgo"
 
 func main() {
-	fmt.Println("vim-go")
+	const token = "MjQ0OTA3NTYyMzQ3MTM0OTc3.CwEXhw.VYdUT4dllK2wVm2UVsucWKFQ6FY"
+	if token == "" {
+		fmt.Println("No bot token specified. Exiting...")
+		return
+	}
+	discord, err := discordgo.New("Bot " + token)
+	// Open the websocket and begin listening.
+	err = discord.Open()
+	if err != nil {
+		fmt.Println("Error opening Discord session: ", err)
+	}
+	fmt.Println("Discord-CRM-Link is now running. Press CTRL-C to exit.")
+	<-make(chan struct{})
+	return
 }
