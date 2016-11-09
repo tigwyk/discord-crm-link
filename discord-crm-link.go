919396e64c741d988747a56069340c815cd73533
Lee Ingram <lee@soylentcola.com>
2016-11-09 22:15:14 +0000

    More discord integration

--- discord-crm-link.go
+++ discord-crm-link.go
@@ -1,7 +1,12 @@
 package main
 
-import "fmt"
-import "github.com/bwmarrin/discordgo"
+import (
+	"fmt"
+	"github.com/bwmarrin/discordgo"
+	"strings"
+)
+
+var buffer = make([][]byte, 0)
 
 func main() {
 	const token = "MjQ0OTA3NTYyMzQ3MTM0OTc3.CwEXhw.VYdUT4dllK2wVm2UVsucWKFQ6FY"
@@ -10,6 +15,10 @@ func main() {
 		return
 	}
 	discord, err := discordgo.New("Bot " + token)
+
+	discord.AddHandler(ready)
+	discord.AddHandler(messageCreate)
+
 	// Open the websocket and begin listening.
 	err = discord.Open()
 	if err != nil {
@@ -18,4 +27,45 @@ func main() {
 	fmt.Println("Discord-CRM-Link is now running. Press CTRL-C to exit.")
 	<-make(chan struct{})
 	return
+}
+
+func ready(s *discordgo.Session, event *discordgo.Ready) {
+	// Set the playing status.
+	_ = s.UpdateStatus(0, "!airhorn")
+}
+
+// This function will be called (due to AddHandler above) every time a new
+// guild is joined.
+func guildCreate(s *discordgo.Session, event *discordgo.GuildCreate) {
+	if event.Guild.Unavailable != nil {
+		return
+	}
+
+	for _, channel := range event.Guild.Channels {
+		if channel.ID == event.Guild.ID {
+			_, _ = s.ChannelMessageSend(channel.ID, "Airhorn is ready! Type !airhorn while in a voice channel to play a sound.")
+			return
+		}
+	}
+}
+
+// This function will be called (due to AddHandler above) every time a new
+// message is created on any channel that the autenticated bot has access to.
+func messageCreate(s *discordgo.Session, m *discordgo.MessageCreate) {
+	if strings.HasPrefix(m.Content, "!airhorn") {
+		// Find the channel that the message came from.
+		c, err := s.State.Channel(m.ChannelID)
+		if err != nil {
+			// Could not find channel.
+			return
+		}
+
+		// Find the guild for that channel.
+		g, err := s.State.Guild(c.GuildID)
+		if err != nil {
+			// Could not find guild.
+			return
+		}
+		fmt.Println(c.Name, g.Name)
+	}
 }
